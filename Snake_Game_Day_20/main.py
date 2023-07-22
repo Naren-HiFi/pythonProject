6c1438d1cfebde6d51a874e20ddd5ffd5617e4eb
N2N-B <rednan69@gmail.com>
2023-07-22 17:03:59 +0530

    Snake_Game_Day_20 Started

--- Snake_Game_Day_20/main.py
+++ Snake_Game_Day_20/main.py
@@ -1,1 +1,9 @@
-print("Snake Game")
+from turtle import Turtle, Screen
+
+ninja = Turtle()
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake game")
+
+screen.exitonclick()
