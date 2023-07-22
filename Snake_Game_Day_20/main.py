0f7a9d92b31859d56a881d06e2d32769f5f71d31
N2N-B <rednan69@gmail.com>
2023-07-22 18:24:38 +0530

    Snake_Game_Day_20 Finished Started Day 21 Inheritance, Slicing, and finishing the snake game

--- Snake_Game_Day_20/main.py
+++ Snake_Game_Day_20/main.py
@@ -1,9 +1,26 @@
 from turtle import Turtle, Screen
+from snake import Snake
+import time
 
-ninja = Turtle()
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake game")
+screen.tracer(0)
+
+my_snake = Snake()
+
+screen.listen()
+screen.onkey(my_snake.up,"Up")
+screen.onkey(my_snake.down,"Down")
+screen.onkey(my_snake.left,"Left")
+screen.onkey(my_snake.right,"Right")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    my_snake.move()
 
 screen.exitonclick()
