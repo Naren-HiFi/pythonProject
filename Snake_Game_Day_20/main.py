ba5ee66ffbb33a4a7975930a7180ab2b7407ba86
N2N-B <rednan69@gmail.com>
2023-07-22 23:58:15 +0530

    Snake_Game_Day_21 Finished Started Day 21 Inheritance, Slicing, and finishing the snake game

--- Snake_Game_Day_20/main.py
+++ Snake_Game_Day_20/main.py
@@ -1,4 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+
+from scoreboard import Scoreboard
+from food import Food
 from snake import Snake
 import time
 
@@ -9,18 +12,33 @@ screen.title("Snake game")
 screen.tracer(0)
 
 my_snake = Snake()
+my_food = Food()
+my_scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(my_snake.up,"Up")
-screen.onkey(my_snake.down,"Down")
-screen.onkey(my_snake.left,"Left")
-screen.onkey(my_snake.right,"Right")
+screen.onkey(my_snake.up, "Up")
+screen.onkey(my_snake.down, "Down")
+screen.onkey(my_snake.left, "Left")
+screen.onkey(my_snake.right, "Right")
 
 is_game_on = True
 while is_game_on:
     screen.update()
     time.sleep(0.1)
-
     my_snake.move()
+
+    if my_snake.head.distance(my_food) < 15:
+        my_food.refresh()
+        my_snake.extend()
+        my_scoreboard.increase_score()
+
+    if my_snake.head.xcor() > 280 or my_snake.head.xcor() < -280 or my_snake.head.ycor() > 280 or my_snake.head.ycor() < -280:
+        is_game_on = False
+        my_scoreboard.game_over()
+
+    for segment in my_snake.segments[1:]:
+        if my_snake.head.distance(segment) < 10:
+            is_game_on = False
+            my_scoreboard.game_over()
 
 screen.exitonclick()
--- Snake_Game_Day_20/snake.py
+++ Snake_Game_Day_20/snake.py
@@ -15,11 +15,18 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITION:
-            snake_body = Turtle("square")
-            snake_body.color("white")
-            snake_body.penup()
-            snake_body.goto(position)
-            self.segments.append(snake_body)
+            self.add_segment(position)
+    def add_segment(self, position):
+        snake_body = Turtle("square")
+        snake_body.color("white")
+        snake_body.penup()
+        snake_body.goto(position)
+        self.segments.append(snake_body)
+
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
